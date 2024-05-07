05d7134f7cfb903c645d331dc36b87481ad90632
unknown <mukaefff@gmail.com>
2024-05-07 23:46:24 +0300

    Задание 2 выполнено

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import Character from 'domain.js';
-import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from 'game.js';
+import character from './domain.js';
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
 
 const game = new Game();
 game.start();
